1c12f570fdd480639f16e4c67a66951fc0a11975
Emka5885 <masiak-emilia@wp.pl>
2023-01-05 19:12:32 +0100

    #17 - całość

--- Action RPG/Enemies/Bat.gd
+++ Action RPG/Enemies/Bat.gd
@@ -20,6 +20,7 @@ var state = CHASE
 onready var sprite = $AnimatedSprite
 onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
+onready var hurtbox = $Hurtbox
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -51,7 +52,7 @@ func seek_player():
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
 	knockback = area.knockback_vector * 120
-
+	hurtbox.create_hit_effect()
 
 func _on_Stats_no_health():
 	queue_free()
